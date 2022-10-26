b90ad34b68aade49b134800cc19bc12d5709fea4
Ap7317 <109688912+Ap7317@users.noreply.github.com>
2022-10-26 23:00:54 +0530

    Add files via upload

---
+++ graphic.py
@@ -0,0 +1,20 @@
+
+import turtle
+t=turtle.Turtle()
+turtle.title("Hearth Shape")
+screen=turtle.Screen()
+screen.bgcolor("black")
+t.color("red")
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
+
+
+
+
+
